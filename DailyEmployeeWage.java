0c3001b145ab843dc0c0898eac41d731db84f64d
jayesh <jayesh.bhosale1812@gmail.com>
2021-10-01 10:11:54 +0530

    first commit

---
+++ DailyEmployeeWage.java
@@ -0,0 +1,16 @@
+public class EmpWageBuilderUC2 {
+public static void main(strings[] args)
+int IS_FULLTIME = 1;
+int EMP_RATE_PER_HOUR = 20;
+int empHrs = 0;
+int empWage = 0;
+double empCheck = Math.florr(MAth.random() * 10) % 2;
+if (empCheck == Is_FULL_TIME)
+    empHrs = 8;
+else
+    empHrs = 0;
+empWage = empHrs * EMP_Rate_PER_HOUR;
+System.out.println("Emp Wage; "" +empWage);
+}
+}
+
---
+++ EmpWage
@@ -0,0 +1,10 @@
+public class EmpWageBuilderUC1{
+public static void main(string[]args){
+int IS_FULL_TIME =1;
+double empCheck = Math.florr(Math.random() * 10) % 2;
+if (empCheck == ISFULL_TIME)
+System.out.println("Employee is Present");
+else
+System.out.println("Employee is absent");
+} 
+}
